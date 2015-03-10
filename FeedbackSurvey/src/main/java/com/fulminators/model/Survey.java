fc38f839cce03c4814a4beea08da9e8bee19f905
fulminator <dansanta2003@yahoo.com>
2015-03-10 03:34:41 +0200

    create survey with primefaces

--- FeedbackSurvey/src/main/java/com/fulminators/model/Survey.java
+++ FeedbackSurvey/src/main/java/com/fulminators/model/Survey.java
@@ -1,18 +1,14 @@
 package com.fulminators.model;
 
-import java.util.ArrayList;
 import java.util.List;
-
-import com.fulminators.dao.SurveyDao;
-import com.fulminators.dao.SurveyDaoImpl;
 
 public class Survey implements java.io.Serializable {
 
 	private static final long serialVersionUID = 1L;
 
 	// private variables ***
-	// SurveyID - unique identifier of a survey
-	private int surveyID;
+	// SurveyId - unique identifier of a survey
+	private int surveyId;
 	// Title - survey name (Java course)
 	private String title;
 	// Description - small summary of the survey
@@ -22,25 +18,13 @@ public class Survey implements java.io.Serializable {
 	// answers of the survey @Transient
 	private List<String> answer;
 
-	// default public constructor (JavaBean required) ***
-	public Survey() {
-
-	}
-
-	// explicit constructor ***
-	public Survey(int surveyID, String title, String description) {
-		this.surveyID = surveyID;
-		this.title = title;
-		this.description = description;
-	}
-
 	// getters and setters ***
-	public int getSurveyID() {
-		return surveyID;
+	public int getSurveyId() {
+		return surveyId;
 	}
 
-	public void setSurveyID(int surveyID) {
-		this.surveyID = surveyID;
+	public void setSurveyId(int surveyId) {
+		this.surveyId = surveyId;
 	}
 
 	public String getTitle() {
@@ -75,46 +59,12 @@ public class Survey implements java.io.Serializable {
 		this.answer = answer;
 	}
 
-	// some functions accessed from jsf ***
-	public void saveSurvey() {
-		SurveyDao dao = new SurveyDaoImpl();
-		dao.save(this);
-		clearAll();
-	}
-
-	public void updateSurvey() {	
-		SurveyDao dao = new SurveyDaoImpl();
-		dao.update(this);
-		clearAll();
-	}
-
-	public void deleteSurvey() {
-		SurveyDao dao = new SurveyDaoImpl();
-		dao.delete(surveyID);
-		clearAll();
-	}
-
-	public List<Survey> getAllSurveys() {
-		List<Survey> surveys = new ArrayList<Survey>();
-		SurveyDao dao = new SurveyDaoImpl();
-		surveys = dao.getAllSurvey();
-		return surveys;
-	}
-
-/*	public void fullInfo() {
-		SurveyDao dao = new SurveyDaoImpl();
-		List<Survey> lc = dao.(selectedname);
-		System.out.println(lc.get(0).firstName);
-		this.custId = lc.get(0).custId;
-		this.firstName = lc.get(0).firstName;
-		this.lastName = lc.get(0).lastName;
-		this.email = lc.get(0).email;
-		this.dob = lc.get(0).dob;
-		this.sd = sdf.format(dob);
-	}*/
-
-	private void clearAll() {
-		this.title = "";
-		this.description = "";
+	@Override
+	public String toString() {
+		StringBuffer strBuff = new StringBuffer();
+		strBuff.append("Id : ").append(this.getSurveyId());
+        strBuff.append(", Title : ").append(this.getTitle());
+        strBuff.append(", Description : ").append(this.getDescription());
+		return strBuff.toString();
 	}
 }
